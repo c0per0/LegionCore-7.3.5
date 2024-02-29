2620272cf03b584d5d3e094b0e1ac89a50dd8f92
SoulSeekkor <soulseekkor@live.com>
2024-02-28 19:34:55 -0600

    Merged files together.

--- src/server/game/Entities/Pet/Pet.h
+++ src/server/game/Entities/Pet/Pet.h
@@ -22,12 +22,13 @@
 #include "Unit.h"
 #include "TemporarySummon.h"
 
-enum ActionFeedback
+enum ActionFeedback : uint8
 {
     FEEDBACK_NONE            = 0,
     FEEDBACK_PET_DEAD        = 1,
     FEEDBACK_NOTHING_TO_ATT  = 2,
-    FEEDBACK_CANT_ATT_TARGET = 3
+    FEEDBACK_CANT_ATT_TARGET = 3,
+    FEEDBACK_NO_PATH         = 4
 };
 
 enum PetTalk
@@ -60,6 +61,95 @@ enum PetNameInvalidReason
 
 #define PET_FOLLOW_DIST  1.0f
 #define PET_FOLLOW_ANGLE (M_PI/2)
+
+enum PetType : uint8
+{
+    SUMMON_PET = 0,
+    HUNTER_PET = 1,
+    MAX_PET_TYPE = 4
+};
+
+#define MAX_ACTIVE_PETS         5
+#define MAX_PET_STABLES         200
+
+// stored in character_pet.slot
+enum PetSaveMode : int16
+{
+    PET_SAVE_AS_DELETED = -2,                        // not saved in fact
+    PET_SAVE_AS_CURRENT = -3,                        // in current slot (with player)
+    PET_SAVE_FIRST_ACTIVE_SLOT = 0,
+    PET_SAVE_LAST_ACTIVE_SLOT = PET_SAVE_FIRST_ACTIVE_SLOT + MAX_ACTIVE_PETS,
+    PET_SAVE_FIRST_STABLE_SLOT = 5,
+    PET_SAVE_LAST_STABLE_SLOT = PET_SAVE_FIRST_STABLE_SLOT + MAX_PET_STABLES, // last in DB stable slot index
+    PET_SAVE_NOT_IN_SLOT = -1                         // for avoid conflict with stable size grow will use negative value
+};
+
+constexpr bool IsActivePetSlot(PetSaveMode slot)
+{
+    return slot >= PET_SAVE_FIRST_ACTIVE_SLOT && slot < PET_SAVE_LAST_ACTIVE_SLOT;
+}
+
+constexpr bool IsStabledPetSlot(PetSaveMode slot)
+{
+    return slot >= PET_SAVE_FIRST_STABLE_SLOT && slot < PET_SAVE_LAST_STABLE_SLOT;
+}
+
+enum PetStableFlags : uint8
+{
+    PET_STABLE_ACTIVE = 0x1,
+    PET_STABLE_INACTIVE = 0x2
+};
+
+enum PetSpellState
+{
+    PETSPELL_UNCHANGED = 0,
+    PETSPELL_CHANGED = 1,
+    PETSPELL_NEW = 2,
+    PETSPELL_REMOVED = 3
+};
+
+enum PetSpellType
+{
+    PETSPELL_NORMAL = 0,
+    PETSPELL_FAMILY = 1,
+    PETSPELL_TALENT = 2
+};
+
+#define PET_FOLLOW_DIST  1.0f
+#define PET_FOLLOW_ANGLE float(M_PI)
+
+enum class PetTameResult : uint8
+{
+    Ok = 0,
+    InvalidCreature = 1,
+    TooMany = 2,
+    CreatureAlreadyOwned = 3,
+    NotTameable = 4,
+    AnotherSummonActive = 5,
+    UnitsCantTame = 6,
+    NoPetAvailable = 7,
+    InternalError = 8,
+    TooHighLevel = 9,
+    Dead = 10,
+    NotDead = 11,
+    CantControlExotic = 12,
+    InvalidSlot = 13,
+    EliteTooHighLevel = 14
+};
+
+enum class StableResult : uint8
+{
+    NotEnoughMoney = 1,                              // "you don't have enough money"
+    InvalidSlot = 3,                              // "That slot is locked"
+    StableSuccess = 8,                              // stable success
+    UnstableSuccess = 9,                              // unstable/swap success
+    BuySlotSuccess = 10,                             // buy slot success
+    CantControlExotic = 11,                             // "you are unable to control exotic creatures"
+    InternalError = 12,                             // "Internal pet error"
+};
+
+constexpr uint32 CALL_PET_SPELL_ID = 883;
+constexpr uint32 PET_SUMMONING_DISORIENTATION = 32752;
 
 class Player;
 
@@ -152,4 +242,58 @@ class Pet : public Guardian
             ASSERT(false);
         }
 };
+
+class PetStable
+{
+public:
+    struct PetInfo
+    {
+        PetInfo() { }
+
+        std::string Name;
+        std::string ActionBar;
+        uint32 PetNumber = 0;
+        uint32 CreatureId = 0;
+        uint32 DisplayId = 0;
+        uint32 Experience = 0;
+        uint32 Health = 0;
+        uint32 Mana = 0;
+        uint32 LastSaveTime = 0;
+        uint32 CreatedBySpellId = 0;
+        uint16 SpecializationId = 0;
+        uint8 Level = 0;
+        ReactStates ReactState = ReactStates(0);
+        PetType Type = MAX_PET_TYPE;
+        bool WasRenamed = false;
+    };
+
+    Optional<uint32> CurrentPetIndex;                               // index into ActivePets or UnslottedPets if highest bit is set
+    std::array<Optional<PetInfo>, MAX_ACTIVE_PETS> ActivePets;      // PET_SAVE_FIRST_ACTIVE_SLOT - PET_SAVE_LAST_ACTIVE_SLOT
+    std::array<Optional<PetInfo>, MAX_PET_STABLES> StabledPets;     // PET_SAVE_FIRST_STABLE_SLOT - PET_SAVE_LAST_STABLE_SLOT
+    std::vector<PetInfo> UnslottedPets;                             // PET_SAVE_NOT_IN_SLOT
+
+    PetInfo* GetCurrentPet() { return const_cast<PetInfo*>(const_cast<PetStable const*>(this)->GetCurrentPet()); }
+    PetInfo const* GetCurrentPet() const
+    {
+        if (!CurrentPetIndex)
+            return nullptr;
+
+        if (Optional<uint32> activePetIndex = GetCurrentActivePetIndex())
+            return ActivePets[*activePetIndex] ? &ActivePets[*activePetIndex].value() : nullptr;
+
+        if (Optional<uint32> unslottedPetIndex = GetCurrentUnslottedPetIndex())
+            return *unslottedPetIndex < UnslottedPets.size() ? &UnslottedPets[*unslottedPetIndex] : nullptr;
+
+        return nullptr;
+    }
+
+    Optional<uint32> GetCurrentActivePetIndex() const { return CurrentPetIndex && ((*CurrentPetIndex & UnslottedPetIndexMask) == 0) ? CurrentPetIndex : std::nullopt; }
+    void SetCurrentActivePetIndex(uint32 index) { CurrentPetIndex = index; }
+    Optional<uint32> GetCurrentUnslottedPetIndex() const { return CurrentPetIndex && ((*CurrentPetIndex & UnslottedPetIndexMask) != 0) ? Optional<uint32>(*CurrentPetIndex & ~UnslottedPetIndexMask) : std::nullopt; }
+    void SetCurrentUnslottedPetIndex(uint32 index) { CurrentPetIndex = index | UnslottedPetIndexMask; }
+
+private:
+    static constexpr uint32 UnslottedPetIndexMask = 0x80000000;
+};
+
 #endif
