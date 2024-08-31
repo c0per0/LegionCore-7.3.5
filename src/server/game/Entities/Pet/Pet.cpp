aa3a1b4eb0de07a2baead334b775b138d623b563
SoulSeekkor <soulseekkor@live.com>
2024-08-31 11:02:03 -0500

    Constructor fix.

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -36,7 +36,7 @@
 #include "WorldPacket.h"
 
 Pet::Pet(Player* owner, PetType type) : Guardian(nullptr, owner, true),
-m_removed(false), m_duration(0), m_specialization(0), m_groupUpdateMask(0), m_declinedname(nullptr)
+m_removed(false), m_duration(0), m_specialization(0), m_declinedname(nullptr)
 {
     m_slot = PET_SLOT_UNK_SLOT;
     m_owner = static_cast<Unit*>(owner);
